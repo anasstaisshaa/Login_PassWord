d4d5c6e95deceff04a0235266844e8f8246622e8
anasstaisshaa <anastaisha.580@gmail.com>
2022-12-06 20:23:24 +0200

    Modified method

--- src/com/JavaCore/Exception/Runner.java
+++ src/com/JavaCore/Exception/Runner.java
@@ -3,23 +3,25 @@ package com.JavaCore.Exception;
 public class Runner {
 
     public static boolean control(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException{
-        boolean onlyLatinAlphabet = login.matches("^[a-zA-Z]+$");
+        boolean onlyLatinAlphabet = login.matches("^[a-zA-Z0-9_]+$");
         boolean LoginConfirm = false;
         boolean PasswordConfirm = false;
         boolean passwordCheck = password.matches("^[a-zA-Z0-9_]+$");
         if(login.length() >= 20 && onlyLatinAlphabet){
             LoginConfirm = true;
         } else{
-            throw new WrongLoginException();
+            throw new WrongLoginException("fidfdivjdiv");
         }
         if(password.length() >= 20 && passwordCheck && password.equals(confirmPassword)){
             PasswordConfirm = true;
         } else{
-            throw new WrongPasswordException();
+            throw new WrongPasswordException("jidjvidvjdivjd");
         }
         try{
             control("uheufhuefu", "huvhduvhduvhduvd","hvduvhduvhduvhd");
-        } catch (WrongLoginException | WrongPasswordException exception){
+        } catch (WrongLoginException exception){
+            exception.printStackTrace();
+        } catch (WrongPasswordException exception){
             exception.printStackTrace();
         }
         return (LoginConfirm && PasswordConfirm);
