401676b6bc4f16b209a1fc2c555819b9ae169a56
anasstaisshaa <anastaisha.580@gmail.com>
2022-12-07 15:21:58 +0200

    refactor exception

--- src/com/JavaCore/Exception/Runner.java
+++ src/com/JavaCore/Exception/Runner.java
@@ -2,31 +2,31 @@ package com.JavaCore.Exception;
 
 public class Runner {
 
-    public static boolean control(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException{
+    public static boolean control(String login, String password, String confirmPassword){
         boolean onlyLatinAlphabet = login.matches("^[a-zA-Z0-9_]+$");
         boolean LoginConfirm = false;
         boolean PasswordConfirm = false;
         boolean passwordCheck = password.matches("^[a-zA-Z0-9_]+$");
-        if(login.length() >= 20 && onlyLatinAlphabet){
-            LoginConfirm = true;
-        } else{
-            throw new WrongLoginException("fidfdivjdiv");
-        }
-        if(password.length() >= 20 && passwordCheck && password.equals(confirmPassword)){
-            PasswordConfirm = true;
-        } else{
-            throw new WrongPasswordException("jidjvidvjdivjd");
+        try{
+            if(login.length() >= 20 && onlyLatinAlphabet){
+                LoginConfirm = true;
+            } else{
+                throw new WrongLoginException("Wrong Login");
+            }
+            if(password.length() >= 20 && passwordCheck && password.equals(confirmPassword)){
+                PasswordConfirm = true;
+            } else{
+                throw new WrongPasswordException("Wrong Password");
+            }
+        } catch (WrongLoginException exception){
+            System.out.println("Exception: " + exception.getMessage());
+        } catch (WrongPasswordException exception){
+            System.out.println("Exception: " + exception.getMessage());
         }
         return (LoginConfirm && PasswordConfirm);
     }
 
     public static void main(String[] args) {
-        try{
-            control("uheufhuefu", "huvhduvhduvhduvd","hvduvhduvhduvhd");
-        } catch (WrongLoginException exception){
-            exception.printStackTrace();
-        } catch (WrongPasswordException exception){
-            exception.printStackTrace();
-        }
+        System.out.println(control("uhvudfhduhdfuhdfjddjdhfvdfvdf", "aaa","aaa"));
     }
 }
